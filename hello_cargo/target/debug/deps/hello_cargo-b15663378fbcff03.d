6be18a2dd725fd948ef4247e0f55a4239b0ad1de
Yuttasak(Yo)Pannawat <yuttasak.pan@bigc.co.th>
2024-05-01 13:25:30 +0700

    feat(init): init project

---
+++ hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d
@@ -0,0 +1,5 @@
+/Users/a2083673/Workspace/Github/rust-sample/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03: src/main.rs
+
+/Users/a2083673/Workspace/Github/rust-sample/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/a2083673/Workspace/Github/rust-sample/hello_cargo/target/debug/hello_cargo: /Users/a2083673/Workspace/Github/rust-sample/hello_cargo/src/main.rs
